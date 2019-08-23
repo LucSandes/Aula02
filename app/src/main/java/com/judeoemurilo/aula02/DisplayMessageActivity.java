e5dbb2561ba8485df113425daf0257937297e20b
Vinicius de Lima Gomes <37003005+vinnilmg@users.noreply.github.com>
2019-08-22 22:37:04 -0300

    Initial commit Falta aparecer a mensagem digitada

---
+++ app/src/main/java/com/judeoemurilo/aula02/DisplayMessageActivity.java
@@ -0,0 +1,20 @@
+package com.judeoemurilo.aula02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        ViewGroup layout = (ViewGroup)
+                findViewById(R.id.activity_display_message);
+
+    }
+
+}
